2e0218d4901d578046e6e281fc6882ae941edcbe
Christian Lowe <christglowe@gmail.com>
2018-05-16 14:59:20 -0500

    followig django tutorial

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class people(models.Model):
+    id = models.AutoField(primary_key=True)
+    person_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    def __str__(self):
+        return self.person_name
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import people
+
+from django.template import loader
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 # Create your views here.
+def detail(request, question_id):
+    latest_person = people.objects.get(pk=1);
+    print(latest_person)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'person': latest_person
+    }
+    return HttpResponse(template.render(context, request))
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
