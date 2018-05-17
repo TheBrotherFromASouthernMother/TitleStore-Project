a0fe332257664d7d0efb0f1f59055cbf928a64fb
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-05-17 16:25:15 -0500

    adjusting field labels

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,3 +4,9 @@ from django.contrib import admin
 from .models import people
 
 admin.site.register(people)
+
+
+from .models import Customer
+from .models import Vehicle
+admin.site.register(Customer)
+admin.site.register(Vehicle)
