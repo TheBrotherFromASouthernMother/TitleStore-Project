6df095cda115a13fa55840458e9586ea60823498
Christian Lowe <christglowe@gmail.com>
2018-05-17 11:59:35 -0500

    figured out user actions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import people
 
-admin.site.register(people)
+def birthday(modelAdmin, request, queryset):
+    print(queryset.filter())
+
+class peopleAdmin(admin.ModelAdmin):
+    list_display = ['person_name', 'age']
+    ordering = ['person_name']
+    actions = [birthday]
+
+
+admin.site.register(people, peopleAdmin)
