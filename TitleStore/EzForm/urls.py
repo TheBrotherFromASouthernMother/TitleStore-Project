18220cb5983bae0c95eab76a013fea3c4063dff1
Erin Thigpen <35975360+thigabiga@users.noreply.github.com>
2018-05-22 10:46:57 -0500

    added views

--- TitleStore/EzForm/urls.py
+++ TitleStore/EzForm/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     # ex: /polls/
     path('', views.index, name='index'),
+    path('customer/', views.customer, name='customer'),
+    path('vehicle/', views.customer, name='vehicle'),
+    path('forms/', views.customer, name='forms'),
+
+    # can remove these when finished building
     # ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
--- TitleStore/EzForm/views.py
+++ TitleStore/EzForm/views.py
@@ -1,27 +1,37 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# from .models import people
+# from django.template import loader
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from django.http import HttpResponse
-
-from .models import people
-
-from django.template import loader
+from . import forms
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'EzForm/index.html')
 
-# Create your views here.
-def detail(request, question_id):
-    latest_person = people.objects.get(pk=1);
-    print(latest_person)
-    template = loader.get_template('polls/index.html')
-    context = {
-        'person': latest_person
-    }
-    return HttpResponse(template.render(context, request))
+def customer(request):
+    return render(request, 'EzForm/customer.html')
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def vehicle(request):
+    return render(request, 'EzForm/vehicle.html')
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+def forms(request):
+    return render(request, 'EzForm/forms.html')
+
+# def detail(request, question_id):
+#     latest_person = people.objects.get(pk=1);
+#     print(latest_person)
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'person': latest_person
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
