7eaa3b860b8cd86af492f4a6e4d2f959089aff88
andytanghr <30758932+andytanghr@users.noreply.github.com>
2018-05-22 14:59:54 -0500

    added template views for customer model

--- TitleStore/EzForm/urls.py
+++ TitleStore/EzForm/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from EzForm.views import CustomerCreate, CustomerUpdate, CustomerDelete
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,5 +9,8 @@ urlpatterns = [
     path('customers/', views.customers, name='customers'),
     path('vehicles/', views.vehicles, name='vehicles'),
     path('forms/', views.forms, name='forms'),
-    # ex: /polls/5/results/
+    
+    path('customer/create/', CustomerCreate.as_view(), name='customer-create'),
+    path('customer/<int:pk>/', CustomerUpdate.as_view(), name='customer-update'),
+    path('customer/<int:pk>/delete', CustomerDelete.as_view(), name='customer-delete'),
 ]
--- TitleStore/EzForm/views.py
+++ TitleStore/EzForm/views.py
@@ -10,8 +10,9 @@ from .models import Customer, Vehicle
 from django.http import HttpRequest
 
 from django.db import models
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-from .models import people
+# from .models import people
 
 def index(request):
     return render(request, 'EzForm/index.html')
@@ -44,3 +45,30 @@ def detail(request):
         
 def forms(request):
     return render(request, 'EzForm/forms.html')
+
+
+
+
+class CustomerCreate(CreateView):
+    model = Customer
+    fields = '__all__'
+
+class CustomerDelete(DeleteView):
+    model = Customer
+    success_url = 'index' # check for correct url
+
+class CustomerUpdate(UpdateView):
+    model = Customer
+    fields = '__all__'
+
+class VehicleCreate(UpdateView):
+    model = Vehicle
+    fields = '__all__'
+
+class VehicleDelete(DeleteView):
+    model = Vehicle
+    success_url = 'index' # check for correct url
+
+class VehicleUpdate(UpdateView):
+    model = Vehicle
+    fields = '__all__'
